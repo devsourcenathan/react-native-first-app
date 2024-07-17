cdf30bda4c5169b43d2830dc4b4915c384eb2cb1
Nathan Tchinda <sadenachbinathan@gmail.com>
2024-07-17 10:43:44 +0100

    Add Home Slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 export default function home() {
   return (
     <View>
-      <Text style={{fontSize: 50, fontFamily: 'outfit-bold'}}>home</Text>
+      <Header />
+
+      <Slider />
     </View>
   )
 }
---
+++ components/Home/Slider.jsx
@@ -0,0 +1,57 @@
+import { View, Text, FlatList, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../configs/FirebaseConfig'
+import {query, collection, getDocs} from 'firebase/firestore'
+import {Colors} from '../../constants/Colors'
+export default function Slider() {  
+
+    const [sliderList, setSliderList] = useState([])
+    const getSliderList = async () => {
+        setSliderList([])
+        const q = query(collection(db, 'Slider'))
+        const querySnapshot = await getDocs(q)
+
+        querySnapshot.docs.forEach((doc) => {
+            setSliderList(prev => [...prev, doc.data()])
+        })
+    }
+
+    useEffect(() => {
+        getSliderList()
+    },[])
+  return (
+    <View>
+      <Text 
+        style={{
+            fontFamily: 'outfit-bold',
+            fontSize: 20,
+            paddingLeft: 20,
+            paddingTop: 20,
+            paddingBottom: 5,
+
+         }}
+      >
+        #Special for you
+      </Text>
+
+
+      <FlatList 
+        data={sliderList}
+        horizontal
+        style={{paddingLeft: 20}}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+            <Image 
+                source={{uri: item.imageUrl}}
+                style={{
+                    width: 300,
+                    height: 150,
+                    borderRadius: 15,
+                    marginRight: 15
+                }}
+            />
+        )}
+      />
+    </View>
+  )
+}
