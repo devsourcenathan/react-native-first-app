27956406044080cc2010743025a2adbf9470c1b7
Nathan Tchinda <sadenachbinathan@gmail.com>
2024-07-17 10:53:19 +0100

    update .env

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -14,6 +14,15 @@ const firebaseConfig = {
   appId: "1:203668388135:web:ef6e17bd4542691f661102"
 };
 
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN, 
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET_ID,
+//   messagingSenderId: process.env.FIREBASE_MSG_SENDER,
+//   appId: process.env.FIREBASE_APP_ID
+// };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
